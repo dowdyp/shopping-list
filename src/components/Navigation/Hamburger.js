3cec034a162c5ba48115ba7719dab93a379a3220
Parker <dowdyp@wit.edu>
2022-02-18 10:23:25 -0500

    starting mobile hamburger menu

--- src/components/Navigation/Hamburger.js
+++ src/components/Navigation/Hamburger.js
@@ -4,7 +4,14 @@ import './hamburger.css';
 
 function HamburgerMenu(props) {
     return (
-        
+        <ul className="hamburger-menu">
+            <li className="hamburger-menu-item">
+            
+            </li>
+            <li className="hamburger-menu-item">
+
+            </li>
+        </ul>
     )
 }
 
