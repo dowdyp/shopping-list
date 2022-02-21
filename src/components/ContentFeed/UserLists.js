0ff79b1f984a048288519c91ea867f64263caf03
Parker <dowdyp@wit.edu>
2022-02-21 03:35:57 -0500

    ugh

--- src/components/ContentFeed/UserLists.js
+++ src/components/ContentFeed/UserLists.js
@@ -8,8 +8,6 @@ function UserLists({setLocation, userLists}) {
         setLocation("Your Lists")
     }, [])
     
-    const {userLists} = props;
-    
     const mapData = (userLists) => {
         const lists = userLists.map(list => 
             <ShoppingList key={list.id} storeName={list.name} total={list.total} numberOfItems={list.numberOfItems} />  )
